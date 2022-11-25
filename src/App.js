badb4df17dca4ee6b8f77984f6da4f6c1d75c26a
Francisco Peña <francisco@ebi.ac.uk>
2022-11-25 00:34:48 +0000

    feat: update App component, handle pagination prev and next buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchQuery from "./components/SearchQuery";
 import SearchResults from "./components/SearchResults";
-import { fetchSearchResults } from './api';
+import { fetchSearchResults } from "./api";
+import usesDidMountEffect from './hooks/useDidMountEffect';
+
+let resultsPerPage = {};
 
 const App = () => {
+  const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [hitCount, setHitCount] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [nextCursorMark, setNextCursorMark] = useState(null);
+
+  const fetchResults = (params) => {
+    if (resultsPerPage[currentPage]) {
+      setResults(resultsPerPage[currentPage])
+    } else {
+      fetchSearchResults(params)
+      .then(data => {
+        setNextCursorMark(data.nextCursorMark);
+        setHitCount(data.hitCount);
+        setResults(data.results);
+        resultsPerPage[currentPage] = data.results;
+      });
+    }
+
+  }
+
+  const resetState = () => {
+    setResults([]);
+    setNextCursorMark(null);
+    setHitCount(null);
+    setCurrentPage(0);
+    resultsPerPage = {};
+  }
+
+  // dont run effect when receiving initial value
+  // will be handled in onSearchResults function
+  usesDidMountEffect(() => {
+    fetchResults({query, cursorMark: nextCursorMark});
+  }, [currentPage]);
+
   const onSearchResults = (query) => {
-    fetchSearchResults(query)
-    .then(results => {
-      setHitCount(results.hitCount);
-      setResults(results.resultList.result);
-    });
+    resetState();
+    setQuery(query);
+    fetchResults({query})
+  }
+  const onNextPage = () => setCurrentPage(currentPage + 1);
+  const onPrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
   }
   return (
     <>
@@ -22,6 +62,9 @@ const App = () => {
       <SearchResults
         results={results}
         hitCount={hitCount}
+        onNextPage={onNextPage}
+        onPrevPage={onPrevPage}
+        currentPage={currentPage}
       >
       </SearchResults>
     </>
